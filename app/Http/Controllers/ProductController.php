cca0805339a91c0d875d4f86a59e88ecf3fd1db3
AlanCasal <foxy.adc@gmail.com>
2018-08-15 23:36:35 -0300

    Edite la ruta, el Redirect y la view mostrarProductos para que el buscador y las categorias ahora desemboquen en mostrarProductos y ambas posibilidades funcionen.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\DB;
 use App\Models\Product;
 
 class ProductController extends Controller {
+
   public static function showPcs() {
     $pcs = Product::where('category_id', '=', 7)->get(['id', 'price', 'description'])->random(3);
     // buscar en tabla products items con category_id=7 (pcs armadas), traer id, precio y descripcion, tres productos al azar
@@ -23,8 +24,72 @@ class ProductController extends Controller {
     return $searchResults;
   }
 
-  public function show($id){
+  public static function show($id){
     $products = Product::where('category_id', '=', $id)->get(['id', 'description', 'price']);
-    return view('/mostrarProductos', ['products' => $products]);
+    return $products;
+    // return view('/mostrarProductos', ['products' => $products]);
   }
+
+  /**
+  * Display a listing of the resource.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function index() {
+  }
+
+  /**
+  * Show the form for creating a new resource.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function create() {
+  }
+
+  /**
+  * Store a newly created resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @return \Illuminate\Http\Response
+  */
+  public function store(Request $request) {
+  }
+
+  /**
+  * Display the specified resource.
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  // public function show($id) {
+  // }
+
+  /**
+  * Show the form for editing the specified resource.
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function edit($id) {
+  }
+
+  /**
+  * Update the specified resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function update(Request $request, $id) {
+  }
+
+  /**
+  * Remove the specified resource from storage.
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function destroy($id) {
+  }
+
 }
